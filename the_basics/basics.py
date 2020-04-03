a22f149a8ac24955f9610a43752005b2036bdb6a
root <hish.b92@gmail.com>
2020-04-03 16:39:34 +0500

    tuples

--- the_basics/basics.py
+++ the_basics/basics.py
@@ -1,3 +1,25 @@
-print(list(range(1,10))) # just need to specify list boundaries for it to display a list
+# monday_temperatures = [9.1,8.8,7.5]
 
-print(list(range(1,10, 2))) # this generates a list by counting every 2 items starting from 1 and ending at 9.
+# student_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.9} 
+
+# mysum = sum(student_grades.values()) # to get the values of the dictionary just do this
+# # mysum = sum(student_grades.keys())  to get the keys of the dictionary just do this
+# length = len(student_grades)
+# mean = mysum/length
+# print(mean)
+
+# student_grades = [10.1,8.8,7.5,10.1,4.5,10.1] # student_grades is the list - list.count() returns the number of times 10.1 appeared 
+# print(student_grades.count(10.1))
+
+# name = "hisham"
+# print(name.lower()) # lower() for lowerclass and upper() for upper class
+
+
+# These are called tuples - with () brackets.
+# monday_temperatures = (1,4,5) # cannot append tuples
+# print(monday_temperatures)
+
+# monday_temperatures2 = [1,4,5]
+# monday_temperatures2.append(6)
+# monday_temperatures2.remove(4)  using remove() method to remove from the list
+# print(monday_temperatures2) # lists are appendable - using append() method to add to the list
