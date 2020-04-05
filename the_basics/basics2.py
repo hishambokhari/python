c9d14c109e57305a386fc93c0209480e0dc0355d
root <hish.b92@gmail.com>
2020-04-05 20:37:35 +0500

    file processing

---
+++ the_basics/basics2.py
@@ -0,0 +1,10 @@
+# myfile = open("fruits.txt") # create a file[]
+# content = myfile.read()
+# myfile.close() # used to close a file
+
+# with open("files/veg.txt", "w") as myfile: #used to create a new file
+#   myfile.write("Tomato\nCucumber\nOnion\n")
+#   myfile.write("garlic")
+
+
+
