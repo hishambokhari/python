3a1b727cee0f534058c2b797574820c5536b5813
root <hish.b92@gmail.com>
2020-04-06 14:03:15 +0500

    Standard modules

--- the_basics/basics2.py
+++ the_basics/basics2.py
@@ -6,5 +6,15 @@
 #   myfile.write("Tomato\nCucumber\nOnion\n")
 #   myfile.write("garlic")
 
+import time
+import os
 
+while True:
+  if os.path.exists("files/veg.txt"):
+    with open("files/veg.txt") as file:
+      print(file.read())
+  else:
+    print("File does not exist.")    
+     
+  time.sleep(5) # freezes script execution for 5 seconds
 
