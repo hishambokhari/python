ec5da37668cca3afb0712036d5ad770ea751fee8
root <hish.b92@gmail.com>
2020-04-06 15:00:32 +0500

    Modules - Pandas

--- the_basics/basics2.py
+++ the_basics/basics2.py
@@ -6,15 +6,30 @@
 #   myfile.write("Tomato\nCucumber\nOnion\n")
 #   myfile.write("garlic")
 
+# import time
+# import os
+
+# while True:
+#   if os.path.exists("files/veg.txt"):
+#     with open("files/veg.txt") as file:
+#       print(file.read())
+#   else:
+#     print("File does not exist.")    
+     
+#   time.sleep(5) # freezes script execution for 5 seconds
+
+
+# Third party modules: Pandas
+
 import time
 import os
+import pandas
 
 while True:
-  if os.path.exists("files/veg.txt"):
-    with open("files/veg.txt") as file:
-      print(file.read())
+  if os.path.exists("files/temps_today.csv"):
+    data = pandas.read_csv("files/temps_today.csv")
+    print(data.mean()["st1"])
   else:
     print("File does not exist.")    
      
-  time.sleep(5) # freezes script execution for 5 seconds
-
+  time.sleep(10) # freezes script execution for 10 seconds
