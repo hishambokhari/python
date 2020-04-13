2756cb2e89f9a0b0a524f4e553b6c8560ea1a16f
root <hish.b92@gmail.com>
2020-04-13 16:46:19 +0500

    for and while loops

---
+++ the_basics/javascript/part_three_hish.js
@@ -0,0 +1,4 @@
+alert("What is the weight in pounds(lbs)?")
+var kgs = prompt("") * 0.454
+alert("That is: " + kgs + " Kilograms")
+console.log("Conversion Completed")
---
+++ the_basics/javascript/whileLoops.js
@@ -0,0 +1,45 @@
+
+// While loops
+// while (x < 5){
+//   console.log("x is currently: "+x);
+
+//   if (x===3) {
+//     console.log("X is equal to 3");
+//     break;
+//   }
+
+
+//   console.log("x is still less than 5, adding 1 to x");
+
+//   x = x+1;
+// }
+
+
+// var num = 1;
+
+// while (num < 16) {
+//   if (num % 2 === 0){
+//     console.log(num);
+//   }
+//   num = num +1;
+  
+// }
+
+// for loops:
+
+// for (var i = 0; i < 5; i++) {
+//   console.log("Number is "+i);
+// }
+
+// var word = "ABCDEFGHIJK"
+
+// for (var i = 0; i < word.length; i++){
+//   console.log(word[i]);
+// }
+
+// var word = "ABABABABAB" // increment by 2
+
+// for (var i = 0; i <word.length; i+=2){
+//   console.log(word[i]);
+// }
+
