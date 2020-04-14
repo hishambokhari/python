2abbec49618a3dc8a64aee1651f3a39751c4cc06
root <hish.b92@gmail.com>
2020-04-14 16:21:35 +0500

    js part 2

---
+++ the_basics/javascript_level_2/functions.js
@@ -0,0 +1,21 @@
+// function helloYou(name){
+//   console.log("Hello " +name);
+// }
+
+// function addNum(num1, num2){
+//   console.log(num1+num2);
+// }
+
+// function helloSomeone(name="Frankie"){
+//   console.log("Hello "+name); // default parameters
+// }
+
+// function formal(name="sam",title="Sir"){
+//   return title + " "+ name
+// }
+
+// function timesFive(numInput){
+//   var result = numInput * 5
+//   return result
+// }
+
