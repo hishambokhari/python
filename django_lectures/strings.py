3437bee1896e863581f5ddb7cb9a96409a0b5154
Hisham Bokhari <hisham.bokhari@cloudasset.com>
2020-05-28 14:46:24 +0500

    Python basics

---
+++ django_lectures/strings.py
@@ -0,0 +1,23 @@
+#basics
+# 'hello'
+# "hello"
+# " I'm a dog "
+
+# mystring = 'Hello World'
+# x = mystring.split()
+# print(x)
+# print(mystring[3]) 
+
+#slicing
+# print(mystring[4:]) #efg
+# print(mystring[:3]) # upto c not including d
+# print(mystring[2:5]) # upto e but not including f
+# print(mystring[:]) # throws all the alphabets
+# print(mystring[::2]) # alternate alphabets 
+
+#print formatting
+
+# x = "Insert another string here: {}".format("INSERT ME!")
+# x = "Item one: {x} Item two: {y}".format(x="dog",y="cat") #multiple formatting
+# print(x)
+
