238d522c66a3afc62df64fa523c11658493e159a
Hisham Bokhari <hisham.bokhari@cloudasset.com>
2020-05-28 15:24:15 +0500

    Dictionaries

---
+++ django_lectures/dict.py
@@ -0,0 +1,10 @@
+# my_stuff = {"key1": "value","key2": 123,"key3":{'123':[1,2,"grabMe"]}}
+# print(my_stuff['key3']['123'][2]
+
+#redefine dict items
+
+# stuff = {"lunch":"pizza","bfast":"eggs"}
+# stuff["lunch"] = "burger"
+# stuff["dinner"] = "pasta"
+# print(stuff)
+
