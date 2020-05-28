5b09edd9549f82fe5e250a5190eb4b6299a39434
Hisham Bokhari <hisham.bokhari@cloudasset.com>
2020-05-28 15:10:27 +0500

    Lists

---
+++ django_lectures/lists.py
@@ -0,0 +1,42 @@
+#lists
+# myList = [1,2,3]
+# myList = ['helloman',2,3,4,5.4,True,[1,2,3]]
+# print(len(myList)) # length of a list
+
+# myList = ['a','b','c']
+
+# print('before_assignment:')
+# print(myList)
+# myList[0] = 'NEW ITEM'
+# print("after assignment:")
+# print(myList)
+
+# myList = ['a','b','c','d','e']
+# myList.append("NEW ITEM")
+# print(myList) # adding to the end of the list
+
+# myListExtend = ['a','b','c','d','e']
+# myListExtend.extend(["x","y","z"])
+# print(myListExtend) # adding a list to a list
+
+# myListPop = ['a','b','c','d','e']
+# item = myListPop.pop(0)
+# print(myListPop) # removes the last letter from the array
+
+# mylist = ['a','b','c']
+# mylist.reverse()
+# print(mylist) # reverse of the list
+
+# mylist = [2,37,4,81,5]
+# mylist.sort()
+# print(mylist) # sorting in order
+
+# mylist = [1,2,['x','y','z']]
+# print(mylist[2][1]) # index a nested 
+
+#list comprehensions
+# matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+# first_col = [row[1] for row in matrix]
+
+# print(first_col) # grabs the second item from each of the rows using a for loop
