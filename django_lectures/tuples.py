6b9cb1b01f4f48f7eb07c0c6745ceb386917aeb6
Hisham Bokhari <hisham.bokhari@cloudasset.com>
2020-05-28 17:01:46 +0500

    Exercise

---
+++ django_lectures/tuples.py
@@ -0,0 +1,25 @@
+#tuples, sets and booleans
+
+#booleans
+# True 
+# False
+
+#Tuples
+# t = (1,2,3) # tuples are immutable
+# print(t[0])
+
+
+#Sets
+# x = set()
+
+# x.add(1)
+# x.add(2)
+
+# print(x)
+
+# con = set([1,1,1,1,1,3,3,3,4,5,6]) # prints repeated elements just once
+
+# print(con)
+
+
+
