c8302660552d7dc27892cb2daa9ea52125893d65
hishambokhari <hish.b92@gmail.com>
2020-09-05 20:10:13 +0100

    strings

---
+++ revise.py
@@ -0,0 +1,32 @@
+
+
+
+# setting variables
+# income = 50
+# tax_rate = 0.1
+
+# income_tax = income * tax_rate
+
+# print (income_tax)
+
+# strings 
+
+# print("I'm a string")
+
+# mystring = 'abcdefg'
+
+# print(mystring[2])
+
+# slicing
+
+mystring = 'hello man how are you'
+
+# print(mystring[:3]) #upto but not including
+
+# print (mystring[1:4]) # from 1 to 4
+
+# x = mystring.split(' ')
+# print(x)
+
+x = "Insert another string: {} Item two: {}".format(mystring, 'dog')
+print(x)
