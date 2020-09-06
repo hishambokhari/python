bbe5dcc054504362cf0fae51737a5e0bd2d65485
hishambokhari <hish.b92@gmail.com>
2020-09-06 23:23:56 +0100

    type

--- revise.py
+++ revise.py
@@ -226,3 +226,11 @@ s = 'django'
 
 # print(result)
 
+def addNum(num1,num2):
+  if type(num1)==type(num2)==type(10):
+    return num1+num2
+  else:
+    return "Sorry I need integers"  
+
+result = addNum(2,3)  
+print(result)
